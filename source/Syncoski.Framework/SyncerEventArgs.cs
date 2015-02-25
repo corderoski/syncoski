c6afc7a859c0110967c6d0e4ef28ca74a02e42d7
jcordero-ede <jcordero-ede@localhost>
2015-02-25 17:04:01 -0400

    - add Logic for auto-detect changes and inform them - update Properties of the EventARgs raised from a change

--- source/Syncoski.Framework/SyncerEventArgs.cs
+++ source/Syncoski.Framework/SyncerEventArgs.cs
@@ -4,5 +4,29 @@ namespace Syncoski.Framework
 {
     public class SyncerEventArgs : EventArgs
     {
+        public SyncerWatcherAction ActionType { get; set; }
+        public String Item { get; set; }
+        public String OldItem { get; set; }
+
+    }
+
+    public enum SyncerWatcherAction
+    {
+        // Summary:
+        //     The creation of a file or folder.
+        Created = 1,
+        //
+        // Summary:
+        //     The deletion of a file or folder.
+        Deleted = 2,
+        //
+        // Summary:
+        //     The change of a file or folder. The types of changes include: changes to
+        //     size, attributes, security settings, last write, and last access time.
+        Changed = 4,
+        //
+        // Summary:
+        //     The renaming of a file or folder.
+        Renamed = 8,
     }
 }
---
+++ source/Syncoski.Framework/SyncerWatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Syncoski.Framework
+{
+    public class SyncerWatcher
+    {
+
+        private readonly FileSystemWatcher _watcher;
+
+        private EventHandler<SyncerEventArgs> _handler;
+
+        public SyncerWatcher()
+        {
+            _watcher = new FileSystemWatcher();
+            _watcher.IncludeSubdirectories = true;
+        }
+
+        public void Register(string folder, EventHandler<SyncerEventArgs> handler)
+        {
+            _watcher.Path = folder;
+            _watcher.EnableRaisingEvents = true;
+            this._handler = handler;
+        }
+
+        public void Listen()
+        {
+            var result = _watcher.WaitForChanged(WatcherChangeTypes.All);
+            //
+            var args = new SyncerEventArgs
+                {
+                    ActionType = (SyncerWatcherAction) ((int) result.ChangeType),
+                    Item = result.Name,
+                    OldItem = result.OldName
+                };
+            _handler(this, args);
+        }
+
+    }
+}
